f9262902f2f6dd64ff2ac7fc6f5bcc398bb35b89
Mr_Zee <65102910+Mr-Zee@users.noreply.github.com>
2021-07-21 22:20:59 +0530

    Add files via upload

---
+++ js/owl-custom.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
